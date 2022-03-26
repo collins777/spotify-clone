9e939929db5fe5cc027e9ee6ac94948c9056bb29
Brandon Collins <brandoncollins@Brandons-MBP.lan>
2022-03-26 15:46:39 -0400

    added access token

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Login } from "./components";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
   // runs code based on a given condition
   useEffect(() => {
-    const token = getTokenFromUrl();
-    
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      // returns user profile
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
+    console.log("I HAVE A TOKEN >>>", token);
   }, []);
 
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in.</h1> : <Login />}</div>
   );
 };
 
