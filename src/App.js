3f541f3a237a8f745f42ac31ef69c3dbfa463d7c
Brandon Collins <brandoncollins@Brandons-MBP.lan>
2022-03-31 21:05:52 -0400

    added data layer to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Login } from "./components";
-import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import { Login, Player } from "./components";
+import { getTokenFromUrl } from "./spotify";
+import { useStateValue } from "./StateProvider";
 
 const spotify = new SpotifyWebApi();
 
 const App = () => {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
 
   // runs code based on a given condition
   useEffect(() => {
@@ -20,16 +22,14 @@ const App = () => {
 
       // returns user profile
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({ type: "SET_USER", user: user });
       });
     }
 
-    console.log("I HAVE A TOKEN >>>", token);
+    // console.log("I HAVE A TOKEN >>>", token);
   }, []);
-
-  return (
-    <div className="app">{token ? <h1>I am logged in.</h1> : <Login />}</div>
-  );
+  console.log(user);
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 };
 
 export default App;
